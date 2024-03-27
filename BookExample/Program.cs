f6c535ccf6e6ab192b2ca2432d665aa49236ff6d
husni <husni@HUSSNI>
2024-03-27 11:41:34 +0100

    Add project files.

---
+++ BookExample/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("My Books");
+List<Books> book = new List<Books>();
+
+Books book1 = new Books("Mark Twain", "Adventures of Tom Sawyer", 200);
+Books book2 = new Books("Lewis Carrol", "Alice in Wonderland", 276);
+Books book3 = new Books("Coleridge", "Ancient Mariner", 554);
+
+book.Add(book1);
+book.Add(book2);
+book.Add(book3);
+Console.ForegroundColor = ConsoleColor.Green;
+foreach (var Books in book)
+{
+    Console.WriteLine(Books.Author + " , " + Books.Name + " , " + Books.Pages+" Pages");
+}
+Console.ReadLine();
+
+
+
+class Books
+{
+    public Books(string author, string name, int pages)
+    {
+        Author = author;
+        Name = name;
+        Pages = pages;
+    }
+
+    public string Author { get; set; }
+    public string Name { get; set; }
+    public int Pages { get; set; }
+}
